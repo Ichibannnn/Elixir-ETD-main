1c1aaa36d61997086293cb39d30240aa9db34b24
Ichibannnn <guser9897@gmail.com>
2025-03-04 13:37:18 +0800

    Update: Miscellaneous Print updated

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -4,20 +4,8 @@ import { decodeUser } from "./decode-user";
 const user = decodeUser();
 
 // Set config defaults when creating the instance
-const request = axios.create({
-  baseURL: "https://localhost:7151/api/",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + user?.token,
-  },
-});
-
-export default request;
-
-//LIVE
-// Set config defaults when creating the instance
 // const request = axios.create({
-//   baseURL: "http://10.10.2.31:72/api/",
+//   baseURL: "https://localhost:7151/api/",
 //   headers: {
 //     "Content-Type": "application/json",
 //     Authorization: "Bearer " + user?.token,
@@ -25,6 +13,18 @@ export default request;
 // });
 
 // export default request;
+
+//LIVE
+// Set config defaults when creating the instance
+const request = axios.create({
+  baseURL: "http://10.10.2.31:72/api/",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + user?.token,
+  },
+});
+
+export default request;
 
 // PRE_TEST
 // const request = axios.create({
